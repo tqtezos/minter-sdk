9142ee95759272036cce5ce573b6f16053c10507
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2021-03-05 15:27:02 -0500

    Include flextesa docker image

---
+++ flextesa/start-sandbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE="registry.gitlab.com/tezos/flextesa:56f651af-run"
+ENTRY="eodbox"
+
+
+docker run --rm --name flextesa-sandbox \
+    -e block_time=5 --detach -p 20000:20000 \
+    "${IMAGE}" "${ENTRY}" start
