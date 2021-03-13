33f24dd17bd6a10accfe706c6c1ac76081c617b5
Rick Love <rick@toldpro.com>
2021-03-13 08:23:35 -0600

    Simplify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
-    extends: 'standard-with-typescript',
+    root: true,
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: './tsconfig.json'
+        project: "./tsconfig.json",
+    },
+    plugins: [
+        '@typescript-eslint',
+        `promise`,
+        'jest',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:jest/recommended',
+        `plugin:promise/recommended`,
+    ],
+    rules: {
+        // Always require semicolon
+        "semi": ["error", "always"],
     }
-}
+};
