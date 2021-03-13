0d14c2d8d6ff9e3e86b9925254cc10825baafe2e
Rick Love <rick@toldpro.com>
2021-03-13 08:40:16 -0600

    Stricter eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,32 @@ module.exports = {
     rules: {
         // Always require semicolon
         "semi": ["error", "always"],
+
+        // Backtick Quotes Everywhere (except jsx and imports)
+        "quotes": ["off", "backtick"],
+        "@typescript-eslint/quotes": ["error", "backtick"],
+        "jsx-quotes": ["error", "prefer-single"],
+        "import-quotes/import-quotes": ["error", "single"],
+
+        // Always Comma Dangle with multiline
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "always-multiline",
+            "exports": "always-multiline",
+            "functions": "always-multiline",
+        }],
+
+        // Use semicolons for multiline types, comma for single line types
+        "@typescript-eslint/member-delimiter-style": ["error", {
+            "multiline": {
+                "delimiter": "semi",
+                "requireLast": true
+            },
+            "singleline": {
+                "delimiter": "comma",
+                "requireLast": false
+            }
+        }],
     }
 };
