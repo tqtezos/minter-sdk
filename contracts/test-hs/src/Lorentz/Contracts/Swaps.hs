bf0d4226862c139cd81d93bb476cf4a9b3819a0e
martoon <martoon.hsk@gmail.com>
2021-03-15 13:58:15 +0300

    Add tests on swaps contract
    Problem: our swaps contract is not yet covered with tests.
    
    Solution: add `Lorentz` bindings and `cleveland` tests with an
    appropriate coverage.
    
    Despite previously `taquito` framework was used for testing other
    contracts, it has been decided to give `cleveland` a try.

--- contracts/test-hs/src/Lorentz/Contracts/Swaps.hs
+++ contracts/test-hs/src/Lorentz/Contracts/Swaps.hs
@@ -1,4 +1,122 @@
 -- | Lorentz bindings for the swaps contract.
 module Lorentz.Contracts.Swaps
-  (
+  ( module Lorentz.Contracts.Swaps
   ) where
+
+import Lorentz
+
+import Lorentz.Contracts.MinterSdk
+import Lorentz.Contracts.Spec.FA2Interface
+import Michelson.Test.Import (embedContractM)
+import qualified Michelson.Typed as T
+
+-- Types
+----------------------------------------------------------------------------
+
+newtype SwapId = SwapId Natural
+  deriving stock (Show, Eq, Ord)
+  deriving newtype (IsoValue, HasAnnotation)
+
+data FA2Token = FA2Token
+  { tokenId :: TokenId
+  , amount :: Natural
+  }
+
+customGeneric "FA2Token" ligoCombLayout
+deriving anyclass instance IsoValue FA2Token
+deriving anyclass instance HasAnnotation FA2Token
+
+data FA2Assets = FA2Assets
+  { fa2Address :: Address
+  , tokens :: [FA2Token]
+  }
+
+customGeneric "FA2Assets" ligoCombLayout
+deriving anyclass instance IsoValue FA2Assets
+deriving anyclass instance HasAnnotation FA2Assets
+
+data SwapStatus
+  = Open
+  | Finished Address
+  | Cancelled
+
+customGeneric "SwapStatus" ligoCombLayout
+deriving anyclass instance IsoValue SwapStatus
+deriving anyclass instance HasAnnotation SwapStatus
+
+data SwapOffer = SwapOffer
+  { assetsOffered :: [FA2Assets]
+  , assetsRequested :: [FA2Assets]
+  }
+
+customGeneric "SwapOffer" ligoCombLayout
+deriving anyclass instance IsoValue SwapOffer
+deriving anyclass instance HasAnnotation SwapOffer
+
+data SwapInfo = SwapInfo
+  { swapOffer :: SwapOffer
+  , seller :: Address
+  , status :: SwapStatus
+  }
+
+customGeneric "SwapInfo" ligoCombLayout
+deriving anyclass instance IsoValue SwapInfo
+deriving anyclass instance HasAnnotation SwapInfo
+
+data SwapEntrypoints
+  = Start SwapOffer
+  | Cancel SwapId
+  | Accept SwapId
+
+customGeneric "SwapEntrypoints" ligoLayout
+deriving anyclass instance IsoValue SwapEntrypoints
+deriving anyclass instance HasAnnotation SwapEntrypoints
+
+instance ParameterHasEntrypoints SwapEntrypoints where
+  type ParameterEntrypointsDerivation SwapEntrypoints = EpdPlain
+
+data SwapStorage = SwapStorage
+  { nextSwapId :: SwapId
+  , swaps :: BigMap SwapId SwapInfo
+  }
+
+customGeneric "SwapStorage" ligoLayout
+deriving anyclass instance IsoValue SwapStorage
+deriving anyclass instance HasAnnotation SwapStorage
+
+initSwapStorage :: SwapStorage
+initSwapStorage = SwapStorage
+  { nextSwapId = SwapId 0
+  , swaps = mempty
+  }
+
+-- This empty slice is a workaround, so that all the declarations above and
+-- their instances may be in the type environment in the TH splice below.
+$(pure [])
+
+-- Contract
+----------------------------------------------------------------------------
+
+swapsContract :: T.Contract (ToT SwapEntrypoints) (ToT SwapStorage)
+swapsContract = $$(embedContractM (inBinFolder "fa2_swap.tz"))
+
+-- Errors
+----------------------------------------------------------------------------
+
+errSwapNotExist :: MText
+errSwapNotExist = [mt|SWAP_NOT_EXIST|]
+
+errSwapFinished :: MText
+errSwapFinished = [mt|SWAP_FINISHED|]
+
+errSwapCancelled :: MText
+errSwapCancelled = [mt|SWAP_CANCELLED|]
+
+errNotSwapSeller :: MText
+errNotSwapSeller = [mt|NOT_SWAP_SELLER|]
+
+errSwapOfferedFA2Invalid :: MText
+errSwapOfferedFA2Invalid = [mt|SWAP_OFFERED_FA2_INVALID|]
+
+errSwapRequestedFA2Invalid :: MText
+errSwapRequestedFA2Invalid = [mt|SWAP_REQUESTED_FA2_INVALID|]
---
+++ contracts/test-hs/test/Test/Swaps/Util.hs
@@ -0,0 +1,116 @@
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+
+module Test.Swaps.Util
+  ( Setup (..)
+  , doSetup
+  , originateFA2
+  , originateSwap
+  , mkFA2Assets
+  ) where
+
+import qualified Data.Foldable as F
+import qualified Data.Map as Map
+import qualified Data.Sized as Sized
+import Data.Type.Ordinal (ordToNatural)
+
+import qualified Indigo.Contracts.FA2Sample as FA2
+import qualified Lorentz.Contracts.Spec.FA2Interface as FA2
+import Lorentz.Value
+import qualified Michelson.Typed as T
+import Morley.Nettest
+
+import Lorentz.Contracts.Swaps
+import Test.Util
+
+-- | Test setup.
+--
+-- We remember sizes of all entries lists in types because that facilitates
+-- allocating exactly as many entities as necessary for the given test suite.
+data Setup addrsNum tokensNum = Setup
+  { sAddresses :: Sized [] addrsNum Address
+  , sTokens :: Sized [] tokensNum FA2.TokenId
+  } deriving stock (Show)
+
+{- | Prepare all the operated entities.
+
+Note that number of addresses and other entities may be inferred automatically,
+so you should bind all the fields of returned 'Setup'. For instance:
+
+@
+scenario = do
+  setup <- doSetup
+  let addr1 ::< addr2 ::< SNil = sAddresses setup
+  -- ↑ Here compiler figures out that exactly 2 addresses should be allocated
+  -- during setup...
+
+  let tokenId :< Nil = sTokens setup
+  -- ↑ ...and only one token.
+
+  ...
+@
+
+Another option is to explicitly annotate the 'doSetup' call:
+
+@
+scenario = do
+  setup <- toSetup @("addresses" :# 2) @("tokens" :# 1)
+  ...
+@
+-}
+doSetup
+  :: forall addrsArg tokensArg addrsNum tokensNum caps base m.
+     ( MonadNettest caps base m
+     , KnownNat addrsNum, addrsArg ~ ("addresses" :# addrsNum)
+     , KnownNat tokensNum, tokensArg ~ ("tokens" :# tokensNum)
+     )
+  => m (Setup addrsNum tokensNum)
+doSetup = do
+  let aliases = Sized.generate' $ \i -> fromString ("fa2-addr-" <> show (ordToNatural i))
+  sAddresses <- mapM newAddress aliases
+  let sTokens = Sized.generate' $ \i -> FA2.TokenId (ordToNatural i)
+  return Setup{..}
+
+-- | Originate a trivial FA2 contract suitable for testing the provided swaps
+-- contract:
+-- * Some money will be put on the addresses from setup, the swaps contract
+--   will be made operator of those addresses.
+-- * The tokenIds from setup will be supported by the originated contract.
+originateFA2
+  :: MonadNettest caps base m
+  => AliasHint
+  -> Setup addrsNum tokensNum
+  -> TAddress SwapEntrypoints
+  -> m (TAddress FA2.FA2SampleParameter)
+originateFA2 name Setup{..} swapContract = do
+  fa2 <- originateSimple name
+    FA2.Storage
+    { sLedger = BigMap $ Map.fromList do
+        -- put money on several tokenIds for each given address
+        addr <- F.toList sAddresses
+        tokenId <- F.toList sTokens
+        pure ((addr, tokenId), 1000)
+    , sOperators = BigMap $ Map.fromList do
+        addr <- F.toList sAddresses
+        pure ((addr, toAddress swapContract), ())
+    , sTokenMetadata = mempty
+    }
+    (FA2.fa2Contract def
+      { FA2.cAllowedTokenIds = F.toList sTokens
+      }
+    )
+  return fa2
+
+-- | Originate the swaps contract.
+originateSwap
+  :: MonadNettest caps base m
+  => m (TAddress SwapEntrypoints)
+originateSwap = do
+  TAddress @SwapEntrypoints <$>
+    originateUntypedSimple "swaps"
+      (T.untypeValue $ T.toVal initSwapStorage)
+      (T.convertContract swapsContract)
+
+-- | Construct 'FA2Assets' from a simplified representation.
+mkFA2Assets :: TAddress fa2Param -> [(FA2.TokenId, Natural)] -> FA2Assets
+mkFA2Assets addr tokens =
+  FA2Assets (toAddress addr) (uncurry FA2Token <$> tokens)
