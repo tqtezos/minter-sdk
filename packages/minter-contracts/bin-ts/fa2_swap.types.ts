77fe4f6d31f8131bce8275ce8ff1b0bf53ebb172
Rick Love <rick@toldpro.com>
2021-03-30 09:27:13 -0500

    Regenerate

---
+++ packages/minter-contracts/bin-ts/fa2_swap.types.ts
@@ -0,0 +1,48 @@
+
+import { address, BigMap, nat } from '@taquito/contract-type-generator';
+
+type Storage = {
+    next_swap_id: nat;
+    swaps: BigMap<nat, {
+        swap_offer: {
+            assets_offered: Array<{
+                fa2_address: address;
+                tokens: Array<{
+                    token_id: nat;
+                    amount: nat;
+                }>;
+            }>;
+            assets_requested: Array<{
+                fa2_address: address;
+                tokens: Array<{
+                    token_id: nat;
+                    amount: nat;
+                }>;
+            }>;
+        };
+        seller: address;
+    }>;
+};
+
+type Methods = {
+    accept: (param: nat) => Promise<void>;
+    cancel: (param: nat) => Promise<void>;
+    start: (params: {
+        assets_offered: Array<{
+            fa2_address: address;
+            tokens: Array<{
+                token_id: nat;
+                amount: nat;
+            }>;
+        }>;
+        assets_requested: Array<{
+            fa2_address: address;
+            tokens: Array<{
+                token_id: nat;
+                amount: nat;
+            }>;
+        }>;
+    }) => Promise<void>;
+};
+
+export type Fa2SwapContractType = { methods: Methods, storage: Storage, code: { __type: 'Fa2SwapCode' } };
