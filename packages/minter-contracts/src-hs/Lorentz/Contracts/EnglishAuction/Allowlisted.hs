deb0cd49a80f2de0e2f2d918197ad3c0f1110481
martoon <martoon.hsk@gmail.com>
2021-04-11 04:15:34 +0300

    Add versions of auction with allowlist
    We need a version of english auction contract that restricts the set
    of allowed FA2s on configuration stage.

---
+++ packages/minter-contracts/src-hs/Lorentz/Contracts/EnglishAuction/Allowlisted.hs
@@ -0,0 +1,89 @@
+-- | Lorentz bindings for the fixed price sale contract
+-- with allowlist.
+module Lorentz.Contracts.EnglishAuction.Allowlisted
+  ( module Lorentz.Contracts.EnglishAuction.Allowlisted
+  ) where
+
+import Lorentz
+
+import Michelson.Test.Import (embedContractM)
+import qualified Michelson.Typed as T
+
+import qualified Lorentz.Contracts.EnglishAuction.FA2 as FA2
+import qualified Lorentz.Contracts.EnglishAuction.Tez as Tez
+import qualified Lorentz.Contracts.EnglishAuction.TezPermit as TezPermit
+import Lorentz.Contracts.MinterSdk
+
+-- Types
+----------------------------------------------------------------------------
+
+data AllowlistedStorage auctionStorage = AllowlistedStorage
+  { auctionStorage :: auctionStorage
+  , allowlist :: BigMap Address ()
+  }
+
+customGeneric "AllowlistedStorage" ligoLayout
+deriving anyclass instance
+  IsoValue s =>
+  IsoValue (AllowlistedStorage s)
+deriving anyclass instance
+  HasAnnotation s =>
+  HasAnnotation (AllowlistedStorage s)
+
+initAllowlistedStorage :: auctionStorage -> AllowlistedStorage auctionStorage
+initAllowlistedStorage ms = AllowlistedStorage ms mempty
+
+data AllowlistedEntrypoints auctionEntrypoints
+  = Call_auction auctionEntrypoints
+  | Update_allowed (BigMap Address ())
+
+customGeneric "AllowlistedEntrypoints" ligoCombLayout
+deriving anyclass instance
+  IsoValue ep =>
+  IsoValue (AllowlistedEntrypoints ep)
+deriving anyclass instance
+  HasAnnotation ep =>
+  HasAnnotation (AllowlistedEntrypoints ep)
+
+instance ParameterHasEntrypoints (AllowlistedEntrypoints FA2.AuctionEntrypoints) where
+  type ParameterEntrypointsDerivation (AllowlistedEntrypoints FA2.AuctionEntrypoints) = EpdRecursive
+
+instance ParameterHasEntrypoints (AllowlistedEntrypoints Tez.AuctionEntrypoints) where
+  type ParameterEntrypointsDerivation (AllowlistedEntrypoints Tez.AuctionEntrypoints) = EpdRecursive
+
+instance ParameterHasEntrypoints (AllowlistedEntrypoints TezPermit.PermitAuctionEntrypoints) where
+  type ParameterEntrypointsDerivation (AllowlistedEntrypoints TezPermit.PermitAuctionEntrypoints) = EpdRecursive
+
+-- This empty slice is a workaround, so that all the declarations above and
+-- their instances may be in the type environment in the TH splice below.
+$(pure [])
+
+-- Contract
+----------------------------------------------------------------------------
+
+auctionFA2AllowlistedContract
+  :: T.Contract
+      (ToT (AllowlistedEntrypoints FA2.AuctionEntrypoints))
+      (ToT (AllowlistedStorage FA2.AuctionStorage))
+auctionFA2AllowlistedContract =
+  $$(embedContractM (inBinFolder "english_auction_fa2_allowlisted.tz"))
+
+auctionTezAllowlistedContract
+  :: T.Contract
+      (ToT (AllowlistedEntrypoints Tez.AuctionEntrypoints))
+      (ToT (AllowlistedStorage Tez.AuctionStorage))
+auctionTezAllowlistedContract =
+  $$(embedContractM (inBinFolder "english_auction_tez_allowlisted.tz"))
+
+auctionTezPermitAllowlistedContract
+  :: T.Contract
+      (ToT (AllowlistedEntrypoints TezPermit.PermitAuctionEntrypoints))
+      (ToT (AllowlistedStorage TezPermit.PermitAuctionStorage))
+auctionTezPermitAllowlistedContract =
+  $$(embedContractM (inBinFolder "english_auction_tez_permit_allowlisted.tz"))
+
+-- Errors
+----------------------------------------------------------------------------
+
+assetAddressNotAllowed :: MText
+assetAddressNotAllowed = [mt|ASSET_ADDRESS_NOT_ALLOWED|]
---
+++ packages/minter-contracts/src-hs/Lorentz/Contracts/EnglishAuction/FA2.hs
@@ -0,0 +1,146 @@
+-- | Lorentz bindings for the english auction (FA2 version).
+module Lorentz.Contracts.EnglishAuction.FA2
+  ( module Lorentz.Contracts.EnglishAuction.FA2
+  ) where
+
+import Lorentz
+
+import Lorentz.Contracts.MinterSdk
+import Lorentz.Contracts.PausableAdminOption
+import Lorentz.Contracts.Spec.FA2Interface
+import Michelson.Test.Import (embedContractM)
+import qualified Michelson.Typed as T
+
+-- Types
+----------------------------------------------------------------------------
+
+data FA2Token = FA2Token
+  { tokenId :: TokenId
+  , amount :: Natural
+  }
+
+customGeneric "FA2Token" ligoCombLayout
+deriving anyclass instance IsoValue FA2Token
+deriving anyclass instance HasAnnotation FA2Token
+
+data Tokens = Tokens
+  { fa2Address :: Address
+  , fa2Batch :: [FA2Token]
+  }
+
+customGeneric "Tokens" ligoCombLayout
+deriving anyclass instance IsoValue Tokens
+deriving anyclass instance HasAnnotation Tokens
+
+data BidCurrency = BidCurrency
+  { fa2Address :: Address
+  , tokenId :: TokenId
+  }
+
+customGeneric "BidCurrency" ligoCombLayout
+deriving anyclass instance IsoValue BidCurrency
+deriving anyclass instance HasAnnotation BidCurrency
+
+data Auction = Auction
+  { seller :: Address
+  , currentBid :: Natural
+  , startTime :: Timestamp
+  , lastBidTime :: Timestamp
+  , roundTime  :: Integer
+  , extendTime  :: Integer
+  , asset :: [Tokens]
+  , minRaisePercent :: Natural
+  , minRaise :: Natural
+  , endTime :: Timestamp
+  , highestBidder :: Address
+  }
+
+customGeneric "Auction" ligoCombLayout
+deriving anyclass instance IsoValue Auction
+deriving anyclass instance HasAnnotation Auction
+
+data ConfigureParam = ConfigureParam
+  { openingPrice :: Natural
+  , minRaisePercent :: Natural
+  , minRaise :: Natural
+  , roundTime :: Natural
+  , extendTime :: Natural
+  , asset :: [Tokens]
+  , startTime :: Timestamp
+  , endTime :: Timestamp
+  }
+
+customGeneric "ConfigureParam" ligoCombLayout
+deriving anyclass instance IsoValue ConfigureParam
+deriving anyclass instance HasAnnotation ConfigureParam
+
+defConfigureParam :: ConfigureParam
+defConfigureParam = ConfigureParam
+  { openingPrice = 1
+  , minRaisePercent = 1
+  , minRaise = 1
+  , roundTime = 100000000000
+  , extendTime = 100000000000
+  , asset = []
+  , startTime = timestampFromSeconds 0
+  , endTime = timestampFromSeconds 1000000000000
+  }
+
+data BidParam = BidParam
+  { assetId :: Natural
+  , bidAmount :: Natural
+  }
+
+customGeneric "BidParam" ligoCombLayout
+deriving anyclass instance IsoValue BidParam
+deriving anyclass instance HasAnnotation BidParam
+
+data AuctionStorage = AuctionStorage
+  { pausableAdmin :: AdminStorage
+  , currentId :: Natural
+  , maxAuctionTime :: Natural
+  , maxConfigToStartTime :: Natural
+  , bidCurrency :: BidCurrency
+  , auctions :: BigMap Natural Auction
+  }
+
+customGeneric "AuctionStorage" ligoCombLayout
+deriving anyclass instance IsoValue AuctionStorage
+deriving anyclass instance HasAnnotation AuctionStorage
+
+initAuctionStorage :: AdminStorage -> BidCurrency -> AuctionStorage
+initAuctionStorage as bc = AuctionStorage
+  { pausableAdmin = as
+  , currentId = 0
+  , maxAuctionTime = 99999999999999999999
+  , maxConfigToStartTime = 99999999999999999
+  , bidCurrency = bc
+  , auctions = mempty
+  }
+
+data AuctionEntrypoints
+  = Configure ConfigureParam
+  | Bid BidParam
+  | Cancel Natural
+  | Resolve Natural
+  | Admin AdminEntrypoints
+
+customGeneric "AuctionEntrypoints" ligoLayout
+deriving anyclass instance IsoValue AuctionEntrypoints
+deriving anyclass instance HasAnnotation AuctionEntrypoints
+
+instance ParameterHasEntrypoints AuctionEntrypoints where
+  type ParameterEntrypointsDerivation AuctionEntrypoints = EpdRecursive
+
+-- This empty slice is a workaround, so that all the declarations above and
+-- their instances may be in the type environment in the TH splice below.
+$(pure [])
+
+-- Contract
+----------------------------------------------------------------------------
+
+englishAuctionFA2Contract :: T.Contract (ToT AuctionEntrypoints) (ToT AuctionStorage)
+englishAuctionFA2Contract = $$(embedContractM (inBinFolder "english_auction_fa2.tz"))
+
+-- Errors
+----------------------------------------------------------------------------
