8bbff6b8249631b22186668531311bf3c97075b7
martoon <martoon.hsk@gmail.com>
2021-04-12 16:43:35 +0300

    Update `index.ts`

--- packages/minter-contracts/index.ts
+++ packages/minter-contracts/index.ts
@@ -1,16 +1,20 @@
 import EditionsTokenMetadataViewCode, { EditionsTokenMetadataViewCodeType } from './bin-ts/editions_token_metadata_view.tz';
 import EnglishAuctionFa2Code, { EnglishAuctionFa2CodeType } from './bin-ts/english_auction_fa2.tz';
+import EnglishAuctionFa2AllowlistedCode, { EnglishAuctionFa2AllowlistedCodeType } from './bin-ts/english_auction_fa2_allowlisted.tz';
 import EnglishAuctionTezCode, { EnglishAuctionTezCodeType } from './bin-ts/english_auction_tez.tz';
+import EnglishAuctionTezAllowlistedCode, { EnglishAuctionTezAllowlistedCodeType } from './bin-ts/english_auction_tez_allowlisted.tz';
 import EnglishAuctionTezPermitCode, { EnglishAuctionTezPermitCodeType } from './bin-ts/english_auction_tez_permit.tz';
+import EnglishAuctionTezPermitAllowlistedCode, { EnglishAuctionTezPermitAllowlistedCodeType } from './bin-ts/english_auction_tez_permit_allowlisted.tz';
+import Fa2AllowlistedSwapCode, { Fa2AllowlistedSwapCodeType } from './bin-ts/fa2_allowlisted_swap.tz';
 import Fa2MultiFtAssetCode, { Fa2MultiFtAssetCodeType } from './bin-ts/fa2_multi_ft_asset.tz';
 import Fa2MultiFtFaucetCode, { Fa2MultiFtFaucetCodeType } from './bin-ts/fa2_multi_ft_faucet.tz';
 import Fa2MultiNftAssetCode, { Fa2MultiNftAssetCodeType } from './bin-ts/fa2_multi_nft_asset.tz';
 import Fa2MultiNftFaucetCode, { Fa2MultiNftFaucetCodeType } from './bin-ts/fa2_multi_nft_faucet.tz';
-import Fa2MultiNftTokenEditionsCode, { Fa2MultiNftTokenEditionsCodeType } from './bin-ts/fa2_multi_nft_token_editions.tz';
 import Fa2SwapCode, { Fa2SwapCodeType } from './bin-ts/fa2_swap.tz';
-import Fa2AllowlistedSwapCode, { Fa2AllowlistedSwapCodeType } from './bin-ts/fa2_allowlisted_swap.tz';
 import FixedPriceSaleMarketCode, { FixedPriceSaleMarketCodeType } from './bin-ts/fixed_price_sale_market.tz';
+import FixedPriceSaleMarketAllowlistedCode, { FixedPriceSaleMarketAllowlistedCodeType } from './bin-ts/fixed_price_sale_market_allowlisted.tz';
 import FixedPriceSaleMarketTezCode, { FixedPriceSaleMarketTezCodeType } from './bin-ts/fixed_price_sale_market_tez.tz';
+import FixedPriceSaleMarketTezAllowlistedCode, { FixedPriceSaleMarketTezAllowlistedCodeType } from './bin-ts/fixed_price_sale_market_tez_allowlisted.tz';
 import TicketAuctionCode, { TicketAuctionCodeType } from './bin-ts/ticket_auction.tz';
 import TicketWalletCode, { TicketWalletCodeType } from './bin-ts/ticket_wallet.tz';
 
@@ -19,10 +23,18 @@ export {
   EditionsTokenMetadataViewCodeType,
   EnglishAuctionFa2Code,
   EnglishAuctionFa2CodeType,
+  EnglishAuctionFa2AllowlistedCode,
+  EnglishAuctionFa2AllowlistedCodeType,
   EnglishAuctionTezCode,
   EnglishAuctionTezCodeType,
+  EnglishAuctionTezAllowlistedCode,
+  EnglishAuctionTezAllowlistedCodeType,
   EnglishAuctionTezPermitCode,
   EnglishAuctionTezPermitCodeType,
+  EnglishAuctionTezPermitAllowlistedCode,
+  EnglishAuctionTezPermitAllowlistedCodeType,
+  Fa2AllowlistedSwapCode,
+  Fa2AllowlistedSwapCodeType,
   Fa2MultiFtAssetCode,
   Fa2MultiFtAssetCodeType,
   Fa2MultiFtFaucetCode,
@@ -31,16 +43,16 @@ export {
   Fa2MultiNftAssetCodeType,
   Fa2MultiNftFaucetCode,
   Fa2MultiNftFaucetCodeType,
-  Fa2MultiNftTokenEditionsCode,
-  Fa2MultiNftTokenEditionsCodeType,
   Fa2SwapCode,
   Fa2SwapCodeType,
-  Fa2AllowlistedSwapCode,
-  Fa2AllowlistedSwapCodeType,
   FixedPriceSaleMarketCode,
   FixedPriceSaleMarketCodeType,
+  FixedPriceSaleMarketAllowlistedCode,
+  FixedPriceSaleMarketAllowlistedCodeType,
   FixedPriceSaleMarketTezCode,
   FixedPriceSaleMarketTezCodeType,
+  FixedPriceSaleMarketTezAllowlistedCode,
+  FixedPriceSaleMarketTezAllowlistedCodeType,
   TicketAuctionCode,
   TicketAuctionCodeType,
   TicketWalletCode,
