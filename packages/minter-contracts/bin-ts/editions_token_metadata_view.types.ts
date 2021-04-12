1570da8eaf69f11890e80230a43d34c4b8cc1234
Rick Love <rick@toldpro.com>
2021-04-12 18:28:44 -0500

    Rebuild

---
+++ packages/minter-contracts/bin-ts/editions_token_metadata_view.types.ts
@@ -0,0 +1,12 @@
+
+import {  } from './type-aliases';
+
+type Storage = {
+    
+};
+
+type Methods = {
+    
+};
+
+export type EditionsTokenMetadataViewContractType = { methods: Methods, storage: Storage, code: { __type: 'EditionsTokenMetadataViewCode', protocol: string, code: unknown } };
