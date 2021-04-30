c910448e7a2fce0baa19f5a3f179b2358cf93343
Zach Brown <zach@zb.dev>
2021-04-30 10:13:09 -0700

    run jest tests against florence

--- flextesa/start-sandbox.sh
+++ flextesa/start-sandbox.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IMAGE="tqtezos/flextesa:20210216"
-ENTRY="edobox"
+IMAGE="tqtezos/flextesa:20210316"
+ENTRY="flobox"
 
-eve="$(docker run tqtezos/flextesa:20210216 edobox flextesa key eve)"
+eve="$(docker run tqtezos/flextesa:20210316 flobox flextesa key eve)"
 
 docker run --rm --name flextesa-sandbox \
     -e block_time=5 --detach -p 20000:20000 \
