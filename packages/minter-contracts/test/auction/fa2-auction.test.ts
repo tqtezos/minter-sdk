879c5205af4167a46565fb61511ffa74b0f7ae01
Eli Guenzburger <eli3720@gmail.com>
2021-06-07 15:40:50 -0400

    increase fa2 auction test timeout

--- packages/minter-contracts/test/auction/fa2-auction.test.ts
+++ packages/minter-contracts/test/auction/fa2-auction.test.ts
@@ -16,7 +16,7 @@ import { addOperator } from '../../src/fa2-interface';
 import { Fa2_token, Tokens, sleep } from '../../src/auction-interface';
 import { queryBalancesWithLambdaView, getBalances, QueryBalances, hasTokens } from '../../test/fa2-balance-inspector';
 
-jest.setTimeout(300000); // 5 minutes
+jest.setTimeout(360000); // 6 minutes
 
 describe('test NFT auction', () => {
   let tezos: TestTz;
