6ec76f638e1fa70184d5cb282e37bd64ee9c4b19
Eli Guenzburger <34725630+EGuenz@users.noreply.github.com>
2021-08-25 14:46:46 -0400

    added english auction allowlisted cancel only admin (#191)

---
+++ packages/minter-contracts/ligo/src/english_auction/english_auction_tez_allowlisted_token_cancel_only_admin.mligo
@@ -0,0 +1,5 @@
+#if !CANCEL_ONLY_ADMIN
+#define CANCEL_ONLY_ADMIN
+#include "../../fa2_modules/fa2_allowlist/allowlist_token.mligo"
+#include "english_auction_tez.mligo"
+#endif
--- packages/minter-contracts/src/compile-ligo.ts
+++ packages/minter-contracts/src/compile-ligo.ts
@@ -334,6 +334,12 @@ const compileSources: CompileSourceEntry[] = [
     contract: true,
   },
   {
+    srcFile: 'english_auction/english_auction_tez_allowlisted_token_cancel_only_admin.mligo',
+    mainFn: 'english_auction_tez_main',
+    dstFile: 'english_auction_tez_allowlisted_token_cancel_only_admin.tz',
+    contract: true,
+  },
+  {
     srcFile: 'english_auction/english_auction_fa2_cancel_only_admin.mligo',
     mainFn: 'english_auction_fa2_main',
     dstFile: 'english_auction_fa2_cancel_only_admin.tz',
