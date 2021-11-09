293215f697d01a992b911c49dffdc4a9ff2b17e8
Eli Guenzburger <eli3720@gmail.com>
2021-11-09 17:24:42 -0500

    fixup! Merge branch 'main' into eli/swap-with-burn

--- packages/minter-contracts/test-hs/Test/Util.hs
+++ packages/minter-contracts/test-hs/Test/Util.hs
@@ -181,7 +181,7 @@ assertingBalanceDeltas fa2 indicedDeltas action = do
   return res
     where
       pullBalance
-        :: MonadNettest caps base m
+        :: MonadNettest base caps m
         => TAddress [FA2.BalanceResponseItem] -> m ()
       pullBalance consumer = do
         let tokenRefs = map fst indicedDeltas
