2e8b56f64eb9270d7369777ee6db515c3b0c1e22
Eli Guenzburger <eli3720@gmail.com>
2022-08-04 18:11:54 -0400

    correctly generating random packs and initializing fa2 contracts

--- packages/minter-contracts/test-hs/Test/Booster/Redeemer.hs
+++ packages/minter-contracts/test-hs/Test/Booster/Redeemer.hs
@@ -5,6 +5,7 @@ import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Foldable as F
 import qualified Data.Set as Set 
+import Data.List.Split as Split
 
 import Hedgehog (Gen, Property, forAll, property)
 import qualified Hedgehog.Gen as Gen
@@ -13,10 +14,11 @@ import Morley.Nettest
 
 import Hedgehog.Gen.Tezos.Core (genMutez')
 import qualified Indigo.Contracts.FA2Sample as FA2
-import Lorentz hiding (balance, contract, sha256)
+import Lorentz hiding (balance, contract, sha256, get)
 import Michelson.Interpret.Pack
 
 import qualified Lorentz.Contracts.Booster.Redeemer as Booster
+import qualified Lorentz.Contracts.Booster.Contracts as BoosterContract
 import Lorentz.Contracts.Spec.FA2Interface (TokenId(TokenId))
 import qualified Lorentz.Contracts.Spec.FA2Interface as FA2
 import Test.Util
@@ -35,13 +37,16 @@ newtype TestData = TestData
 data Setup = Setup
   { seller :: Address
   , buyer :: Address
-  , assetFA2 :: ContractHandler FA2.FA2SampleParameter FA2.Storage
-  , storage :: Booster.BoosterStorage
+  , cardFA2 :: ContractHandler FA2.FA2SampleParameter FA2.Storage
+  , packFA2 :: ContractHandler FA2.FA2SampleParameter FA2.Storage
+  , boosterContract :: ContractHandler Booster.BoosterEntrypoints Booster.BoosterStorage
   }
 
 genTestData :: Gen TestData
 genTestData = do
-  packs <- replicateM 10 (Gen.list (Range.linear 1 5) (Gen.integral (Range.linear 0 9))) --generates a list of 1 to 5 tokens having ids 0 to 9, 10 times
+  shuffledTokenIds :: [Natural] <- Gen.shuffle [0 .. 99]
+  splitLengths <- replicateM 10 (Gen.int (Range.linear 1 10))
+  let packs = Split.splitPlaces splitLengths shuffledTokenIds -- generates 10 random packs of lengths 1 to 10 each 
   nonces <- replicateM 10 (Gen.integral (Range.linear 1 1000000)) -- generates 10 nonces   
   let redeemKeys =  ([1 .. 10] `zip` packs `zip` nonces) <&> (\((packId, tokens), nonce) -> 
           Booster.RedeemKey 
@@ -59,37 +64,44 @@ testSetup testData = do
   seller <- newAddress "seller"
   buyer <- newAddress "buyer"
 
-  let storage =
+  let boosterStorage =
         Booster.initBoosterStorage $
          SimpleAdmin.initAdminStorage seller
 
+  let tokenIds = TokenId `List.map` [0..99]
+  let packIds = TokenId `List.map` [0..9]
+  let ledger =
+        Map.fromListWith (+) $
+          tokenIds <&> \tokenId -> ((seller, tokenId), 1)
+  let packLedger =
+        Map.fromListWith (+) $
+          packIds <&> \packId -> ((buyer, packId), 1)
+  
+  boosterContract <- originateBoosterContract boosterStorage 
 
-  -- Create an FA2 contracts, and give the asset to the seller.
-  assetFA2 <- originateSimple "asset_fa2"
-    FA2.Storage
-    { sLedger = BigMap $ Map.fromList [((seller, TokenId 1), 1)]
-    , sOperators = mempty
-    , sTokenMetadata = mempty
-    }
-    (FA2.fa2Contract def { FA2.cAllowedTokenIds = [TokenId 1] })
+   -- Create an FA2 contracts, and give the card assets to the seller.
+  cardFA2 <- originateSimple "card_fa2"
+     FA2.Storage
+     { sLedger = BigMap ledger
+     , sOperators = BigMap $ Map.fromList [((seller, toAddress boosterContract), ())]
+     , sTokenMetadata = mempty
+     }
+     (FA2.fa2Contract def { FA2.cAllowedTokenIds = tokenIds })
+
+   -- Create an FA2 contracts, and give the pack assets to the buyer
+  packFA2 <- originateSimple "pack_fa2"
+     FA2.Storage
+     { sLedger = BigMap packLedger
+     , sOperators = BigMap $ Map.fromList [((buyer, toAddress boosterContract), ())]
+     , sTokenMetadata = mempty
+     }
+     (FA2.fa2Contract def { FA2.cAllowedTokenIds = packIds })
 
   pure Setup {..}
 
-originateBoosterContract :: MonadNettest caps base m => Setup -> Contract Booster.BoosterEntrypoints Booster.BoosterStorage -> m $ ContractHandler Booster.BoosterEntrypoints Booster.BoosterStorage
-originateBoosterContract Setup{storage, seller, assetFA2} boosterContract = do
-  contract <- originateSimple "booster-redeemer" storage boosterContract
-
-  -- Make the contract an operator for the seller.
-  withSender seller $ do
-    call assetFA2 (Call @"Update_operators")
-      [ FA2.AddOperator FA2.OperatorParam
-          { opOwner = seller
-          , opOperator = toAddress contract
-          , opTokenId = TokenId 1
-          }
-      ]
-
-  pure contract
+originateBoosterContract :: MonadNettest caps base m => Booster.BoosterStorage ->  m $ ContractHandler Booster.BoosterEntrypoints Booster.BoosterStorage
+originateBoosterContract storage = do
+  originateSimple "booster-redeemer" storage BoosterContract.boosterContract
 
 ----------------------------------------------------------------------------
 -- Call entrypoints
